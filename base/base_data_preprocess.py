9737140216cec90dc2e15f0751ae3901626fb841
tsikup <tsiknakisn@gmail.com>
2020-04-15 17:37:45 +0300

    deleted data preprocessing

--- base/base_data_preprocess.py
+++
@@ -1,9 +0,0 @@
-class BasePreprocess(object):
-    def __init__(self):
-        pass
-
-    def preprocess(self):
-        #implement data preprocessing
-        raise NotImplementedError
-
-     #other supporting methods
