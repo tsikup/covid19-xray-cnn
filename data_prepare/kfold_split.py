9bd2f5e45cf43544c041b24ffa6e6db374647b73
Nikos Tsiknakis <tsiknakisn@gmail.com>
2020-04-24 12:54:41 +0300

    added code for test images

--- data_prepare/kfold_split.py
+++ data_prepare/kfold_split.py
@@ -10,15 +10,19 @@ import shutil
 import os
 
 
-datasetDir = '../data/data/dataset/all'
-kfoldDir = '../data/data/dataset/kfold'
+datasetDir = '/home/tsiknakisn/projects/covid19-xray/data/data/dataset/all'
+kfoldDir = '/home/tsiknakisn/projects/covid19-xray/data/data/dataset/kfold'
 
 images_0 = glob(os.path.join(datasetDir, '0/*'))
 images_1 = glob(os.path.join(datasetDir, '1/*'))
 images = np.array(images_0 + images_1)
 
+print("Images shape: {}".format(images.shape))
+
 labels = np.zeros(len(images_0))
 labels = np.concatenate((labels, np.ones(len(images_1))))
+
+print("Labels shape: {}".format(labels.shape))
 
 skf = StratifiedKFold(n_splits=5, shuffle=True)
 
@@ -30,8 +34,10 @@ for train_index, test_index in skf.split(images, labels):
     outputTrainDir = os.path.join(kfoldDir, str(idx), 'train')
     outputTestDir = os.path.join(kfoldDir, str(idx), 'test')
 
-    Path(outputTrainDir).mkdir(parents=True, exist_ok=True)
-    Path(outputTestDir).mkdir(parents=True, exist_ok=True)
+    Path(os.path.join(outputTrainDir, '0')).mkdir(parents=True, exist_ok=True)
+    Path(os.path.join(outputTrainDir, '1')).mkdir(parents=True, exist_ok=True)
+    Path(os.path.join(outputTestDir, '0')).mkdir(parents=True, exist_ok=True)
+    Path(os.path.join(outputTestDir, '1')).mkdir(parents=True, exist_ok=True)
 
     for idy, image in enumerate(X_train):
         try:
@@ -41,4 +47,11 @@ for train_index, test_index in skf.split(images, labels):
         except Exception as e:
             print(e)
 
+    for idy, image in enumerate(X_test):
+      try:
+          imageName = os.path.basename(image)
+          filepath = os.path.join(outputTestDir, str(int(y_test[idy])), imageName)
+          shutil.copy2(image,filepath)
+      except Exception as e:
+          print(e)
     idx = idx + 1
